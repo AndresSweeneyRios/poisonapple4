a71db0696d4189ab18f4871658054c6f84a7e7c2
Andr3wRiv3rs <andrewarivers@gmail.com>
2024-04-14 17:25:39 -0700

    Add missing technologies

---
+++ src/assets/technologies/index.ts
@@ -0,0 +1,125 @@
+// A map of all the technologies used in the project and their corresponding icons
+
+import Android from './android.svg'
+import AWS from './aws.svg'
+import Bitcoin from './bitcoin.svg'
+import BitcoinLightning from './bitcoin_lightning.svg'
+import Blockcypher from './blockcypher.svg'
+import Blocknative from './blocknative.svg'
+import Bootstrap from './bootstrap.svg'
+import Bulma from './bulma.svg'
+import CSharp from './c#.svg'
+import CSS from './css.svg'
+import DigitalOcean from './digitalocean.svg'
+import DiscordJS from './discordjs.svg'
+import Docker from './docker.svg'
+import Electron from './electron.svg'
+import Ethereum from './ethereum.svg'
+import Express from './express.svg'
+import Figma from './figma.svg'
+import Firebase from './firebase.svg'
+import GamemakerStudio from './gamemaker_studio.svg'
+import GithubActions from './github_actions.svg'
+import GLSL from './glsl.svg'
+import Godot from './godot.svg'
+import HTML from './html.svg'
+import Illustrator from './illustrator.svg'
+import Infura from './infura.svg'
+import Inkscape from './inkscape.svg'
+import JavaScript from './javascript.svg'
+import Java from './java.svg'
+import jQuery from './jquery.svg'
+import Koa from './koa.svg'
+import Kubernetes from './kubernetes.svg'
+import Mollie from './mollie.svg'
+import MongoDB from './mongodb.svg'
+import NGINX from './nginx.svg'
+// import Parcel from './parcel.png'
+import Paypal from './paypal.svg'
+import Phantom from './phantom.svg'
+import Phaser from './phaser.svg'
+import Phonegap from './phonegap.svg'
+import Photoshop from './photoshop.svg'
+import PixiJS from './pixijs.svg'
+import PostCSS from './postcss.svg'
+import React from './react.svg'
+import SASS from './sass.svg'
+import Shopify from './shopify.svg'
+import Solana from './solana.svg'
+import Stripe from './stripe.svg'
+import Stylus from './stylus.svg'
+import Tailwind from './tailwind.svg'
+import ThreeJS from './threejs.svg'
+import TypeScript from './typescript.svg'
+import Unity from './unity.svg'
+import Vue from './vue.svg'
+import Vuetify from './vuetify.svg'
+import Web3 from './web3.svg'
+import WebRTC from './webrtc.svg'
+import Websockets from './websockets.svg'
+import Webpack from './webpack.svg'
+
+interface Technology {
+  name: string,
+  icon: string
+}
+
+export const Technologies: Technology[] = [
+  { name: 'Android', icon: Android },
+  { name: 'AWS', icon: AWS },
+  { name: 'Bitcoin', icon: Bitcoin },
+  { name: 'Bitcoin Lightning', icon: BitcoinLightning },
+  { name: 'Blockcypher', icon: Blockcypher },
+  { name: 'Blocknative', icon: Blocknative },
+  { name: 'Bootstrap', icon: Bootstrap },
+  { name: 'Bulma', icon: Bulma },
+  { name: 'C#', icon: CSharp },
+  { name: 'CSS', icon: CSS },
+  { name: 'DigitalOcean', icon: DigitalOcean },
+  { name: 'DiscordJS', icon: DiscordJS },
+  { name: 'Docker', icon: Docker },
+  { name: 'Electron', icon: Electron },
+  { name: 'Ethereum', icon: Ethereum },
+  { name: 'Express', icon: Express },
+  { name: 'Figma', icon: Figma },
+  { name: 'Firebase', icon: Firebase },
+  { name: 'Gamemaker Studio', icon: GamemakerStudio },
+  { name: 'Github Actions', icon: GithubActions },
+  { name: 'GLSL', icon: GLSL },
+  { name: 'Godot', icon: Godot },
+  { name: 'HTML', icon: HTML },
+  { name: 'Illustrator', icon: Illustrator },
+  { name: 'Infura', icon: Infura },
+  { name: 'Inkscape', icon: Inkscape },
+  { name: 'Java', icon: Java },
+  { name: 'JavaScript', icon: JavaScript },
+  { name: 'jQuery', icon: jQuery },
+  { name: 'Koa', icon: Koa },
+  { name: 'Kubernetes', icon: Kubernetes },
+  { name: 'Mollie', icon: Mollie },
+  { name: 'MongoDB', icon: MongoDB },
+  { name: 'NGINX', icon: NGINX },
+  { name: 'Paypal', icon: Paypal },
+  { name: 'Phantom', icon: Phantom },
+  { name: 'Phaser', icon: Phaser },
+  { name: 'Phonegap', icon: Phonegap },
+  { name: 'Photoshop', icon: Photoshop },
+  { name: 'PixiJS', icon: PixiJS },
+  { name: 'PostCSS', icon: PostCSS },
+  { name: 'React', icon: React },
+  { name: 'SASS', icon: SASS },
+  { name: 'Shopify', icon: Shopify },
+  { name: 'Solana', icon: Solana },
+  { name: 'Stripe', icon: Stripe },
+  { name: 'Stylus', icon: Stylus },
+  { name: 'Tailwind', icon: Tailwind },
+  { name: 'ThreeJS', icon: ThreeJS },
+  { name: 'TypeScript', icon: TypeScript },
+  { name: 'Unity', icon: Unity },
+  { name: 'Vue', icon: Vue },
+  { name: 'Vuetify', icon: Vuetify },
+  { name: 'Web3', icon: Web3 },
+  { name: 'WebRTC', icon: WebRTC },
+  { name: 'Websockets', icon: Websockets },
+  { name: 'Webpack', icon: Webpack }
+]
